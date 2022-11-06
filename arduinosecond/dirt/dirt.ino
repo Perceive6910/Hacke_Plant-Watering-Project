d939b8b71847c96e9d9c848420c1ab5593262def
EMLaptop <heatranthegoldrush@proton.me>
2022-11-05 20:43:30 -0600

    Arduino LCD Menu

--- arduinosecond/dirt/dirt.ino
+++ arduinosecond/dirt/dirt.ino
@@ -1,11 +1,11 @@
-const int pinTemp  = A0;
-const int pinUp = A1;
-const int pinDown = A2;
-const int pinMiddle = A3;
+int pinMoisture  = A0;
+int pinUp = A1;
+int pinDown = A2;
+int pinMiddle = A3;
 bool up;
 bool down;
 bool middle;
-int temp;
+int moisture;
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
@@ -17,11 +17,37 @@ void loop() {
 up = ButtonPress(pinUp);
 middle = ButtonPress(pinMiddle);
 down = ButtonPress(pinDown);
-
-  
+moisture = analogRead(pinMoisture);
+moisture = (100-((pinMoisture / 1023)*100));
+Serial.println(moisture);
 Serial.println(up);
 delay(50);
 
+void loop() {
+  // put your main code here, to run repeatedly:
+moisture = analogRead(pinMoisture);
+//moisture = 100-((pinMoisture / 1023)*100);
+Serial.println(moisture);
+delay(50);
+}
+
+
+  delay(2000);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(1);
+  display.setCursor(0,0);
+  display.println("Plant Status");
+  display.setTextSize(1);             
+  display.setTextColor(1);
+  display.setCursor(0,10);
+  display.println("Moisture Percentage");
+  display.setTextSize(1);             
+  display.setTextColor(1);
+  display.setCursor(0,20);
+  display.println("Clock Cycle");
+  display.display();
+  delay(1000);
 
 
 }
@@ -29,7 +55,7 @@ delay(50);
 // converts pin to a bool
 bool ButtonPress(int pin){
   int val = analogRead(pin);
-  if (val == 1023){
+  if (val == 100){
     return true;
   }
   else{
