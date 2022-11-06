8bdab73adbc86f4b3a618ce3d1998fb14e5e9bde
Brandon Mint <bcarpenter5@nait.ca>
2022-11-06 00:15:30 -0600

    added timmer that retruns numbers

---
+++ arduinosecond/waterproject/waterproject/time.ino
@@ -0,0 +1,26 @@
+
+
+String date(int time){
+
+	const int daySeconds =86400;  
+	const int hourSeconds = 3600;
+	const int minuteSeconds = 60;
+	int seconds;
+	int hours;
+	int minutes;
+	int days;
+	String string;
+	
+	days = time / daySeconds;
+	time  = time % daySeconds;
+	hours = time / hourSeconds;
+	time = time / hourSeconds;
+	minutes = time / minuteSeconds;
+	time = time / minuteSeconds;
+	seconds = time;
+	
+	string = ("days " +String(days) + "hours "+String(hours) + "minutes "+ String(minutes) + "seconds " + String(seconds));
+	return string;
+
+
+}
